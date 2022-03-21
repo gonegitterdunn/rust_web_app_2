0afa9ca4c71ffea002cef5ab64ee8d87222ba37f
Gitter Dunn <gitterdunn@protonmail.com>
2022-03-21 21:38:16 +0100

    introduce multiple threads, cell, arc, mutex, atomics

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,17 @@ extern crate actix_web;
 
 use actix_web::{middleware, web, App, HttpRequest, HttpServer, Result};
 use serde::Serialize;
+use std::cell::Cell;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::{Arc, Mutex};
+
+static SERVER_COUNTER: AtomicUsize = AtomicUsize::new(0);
+
+struct AppState {
+  server_id: usize,
+  request_count: Cell<usize>,
+  messages: Arc<Mutex<Vec<String>>>,
+}
 
 pub struct MessageApp {
   port: u16,
@@ -21,25 +32,27 @@ impl MessageApp {
         .service(index)
     })
     .bind(("127.0.0.1", self.port))?
-    .workers(1)
+    .workers(4)
     .run()
   }
 }
 
 #[derive(Serialize)]
 struct IndexResponse {
-  message: String,
+  server_id: usize,
+  request_count: usize,
+  message: Vec<String>,
 }
 
 #[get("/")]
-fn index(req: HttpRequest) -> Result<web::Json<IndexResponse>> {
-  let hello = req
-    .headers()
-    .get("hello")
-    .and_then(|value| value.to_str().ok())
-    .unwrap_or_else(|| "world");
+fn index(state: web::Data<AppState>) -> Result<web::Json<IndexResponse>> {
+  let request_count = state.request_count.get() + 1;
+  state.request_count.set(request_count);
+  let ms = state.messages.lock().unwrap();
 
   Ok(web::Json(IndexResponse {
-    message: hello.to_owned(),
+    server_id: state.server_id,
+    request_count,
+    message: ms.clone(),
   }))
 }
